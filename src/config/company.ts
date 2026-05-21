fc80c4e5cd5f780dc757c1f6e48a129abae03de8
Retheanya <rethanyay2k0@gmail.com>
2026-05-21 10:51:11 +0530

    Update address to shortened version

--- src/config/company.ts
+++ src/config/company.ts
@@ -2,7 +2,7 @@ export const COMPANY = {
   nameLong: 'Enlark Enterprises Private Limited',
   nameShort: 'ENLARK ENTERPRISES',
   address:
-    'SF NO 610/1A, Vadakku Thottam, L&T Road, L & T Road, Vadakku Thottam, Near L & T campus, Malumichampatti, Coimbatore, Tamil Nadu 641050',
+    'SF NO 610/1A, Vadakku Thottam,  Near L & T campus, Malumichampatti, Coimbatore, Tamil Nadu 641050',
   phoneDisplay: '+91 73382 84639',
   phoneHref: '+917338284639',
   email: 'sales@enlark.co.in',
