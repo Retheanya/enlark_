de9cd35d358e5bbb23285ad71934bdd57d61c8a6
Retheanya <rethanyay2k0@gmail.com>
2026-05-21 11:03:26 +0530

    Update google maps embed to single exact place pin

--- src/config/company.ts
+++ src/config/company.ts
@@ -9,6 +9,6 @@ export const COMPANY = {
   emailSales: 'sales@enlark.co.in',
   emailViswa: 'viswa@enlark.co.in',
   mapEmbedSrc:
-    'https://www.google.com/maps?q=SF+NO+610%2F1A,+Vadakku+Thottam,++Near+L+%26+T+campus,+Malumichampatti,+Coimbatore,+Tamil+Nadu+641050&output=embed',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3917.273608858314!2d76.99678657480736!3d10.942692589216382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba85b002cc565af%3A0xf4a5fd48a0cc11f2!2sEnlark+Enterprises+Pvt+Ltd!5e0!3m2!1sen!2sin!4v1779341334963!5m2!1sen!2sin',
 } as const;
 
