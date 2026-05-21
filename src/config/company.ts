56766fbd9420a09ee30388cc62557c70f3ac373a
Retheanya <rethanyay2k0@gmail.com>
2026-05-21 12:40:37 +0530

    Update emails to @enlarkindia.com and add www.enlarkindia.com website support

--- src/config/company.ts
+++ src/config/company.ts
@@ -5,9 +5,10 @@ export const COMPANY = {
     'SF NO 610/1A, Vadakku Thottam,  Near L & T campus, Malumichampatti, Coimbatore, Tamil Nadu 641050',
   phoneDisplay: '+91 73382 84639',
   phoneHref: '+917338284639',
-  email: 'sales@enlark.co.in',
-  emailSales: 'sales@enlark.co.in',
-  emailViswa: 'viswa@enlark.co.in',
+  email: 'sales@enlarkindia.com',
+  emailSales: 'sales@enlarkindia.com',
+  emailViswa: 'viswa@enlarkindia.com',
+  website: 'www.enlarkindia.com',
   mapEmbedSrc:
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3917.273608858314!2d76.99678657480736!3d10.942692589216382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba85b002cc565af%3A0xf4a5fd48a0cc11f2!2sEnlark+Enterprises+Pvt+Ltd!5e0!3m2!1sen!2sin!4v1779341334963!5m2!1sen!2sin',
 } as const;
